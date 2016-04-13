b791d4dfe3bc1eb3a520a181a7049dbbdd40ce3f
Art Garm <artem.garmatyk@gmail.com>
2016-04-13 16:16:30 +0300

    no message

--- src/js/partials/develop_2.js
+++ src/js/partials/develop_2.js
@@ -11,6 +11,17 @@ $(document).ready(function() {
             $(this).find('span').removeClass('background-spec');
         }
     );
+    /*
+        $('.contact-form input').on("focus", function() {
+            $(this).addClass('border-shadow');
+        });
+        $('.contact-form input').off("focus", function() {
+            $(this).removeClass('border-shadow');
+        });
+    */
+    $('.contact-form input').focus(function() { $(this).addClass('border-shadow'); } );
+$('.contact-form input').blur(function() { $(this).removeClass('border-shadow'); })
+
 
 });
 
