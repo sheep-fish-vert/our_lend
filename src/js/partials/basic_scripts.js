7ac8be43a98dee985db22ca913edac55b17303ee
Dmitry <dmitry1939@gmail.com>
2016-04-14 16:21:09 +0300

    mmmm prety girls

--- src/js/partials/basic_scripts.js
+++ src/js/partials/basic_scripts.js
@@ -74,7 +74,7 @@ function randomColors(){
 
     color = Math.floor((Math.random() * 16));
 
-    var randomStyles = '<style data-spec-hover=true>.color-spec{color:#'+colorsMassive[color]+'!important;}.background-spec{background-color:#'+colorsMassive[color]+'!important;}.border-spec{border-color:#'+colorsMassive[color]+'!important;} .border-shadow{border-color:#'+colorsMassive[color]+'!important; box-shadow: 0px 0px 14px 0px #'+colorsMassive[color]+ '; } .color-spec-hover:hover{color: #'+colorsMassiveHover[color]+'!important; }.background-spec-hover:hover{background-color:#'+colorsMassiveHover[color]+'!important; }.border-spec-hover:hover{border-color:#'+colorsMassiveHover[color]+'!important; }.background-spec-reverse{background-color:#'+colorsMassiveHover[color]+'!important; }.background-spec-reverse-hover:hover{background-color:#'+colorsMassive[color]+'!important;}.border-spec-reverse{border-color:#'+colorsMassiveHover[color]+'!important; }.border-spec-reverse-hover:hover{border-color:#'+colorsMassive[color]+'!important; }.svg-spec{fill:#'+colorsMassive[color]+'!important;}.svg-spec-hover:hover{fill:#'+colorsMassiveHover[color]+'!important;}</style > ';
+    var randomStyles = '<style data-spec-hover=true>.color-spec{color:#'+colorsMassive[color]+'!important;}.background-spec{background-color:#'+colorsMassive[color]+'!important;}.border-spec{border-color:#'+colorsMassive[color]+'!important;} .border-shadow{border-color:#'+colorsMassive[color]+'!important; box-shadow: 0px 0px 14px 0px #'+colorsMassive[color]+ '; } .color-spec-hover:hover{color: #'+colorsMassiveHover[color]+'!important; }.background-spec-hover:hover{background-color:#'+colorsMassiveHover[color]+'!important; }.border-spec-hover:hover{border-color:#'+colorsMassiveHover[color]+'!important; }.background-spec-reverse{background-color:#'+colorsMassiveHover[color]+'!important; }.background-spec-reverse-hover:hover{background-color:#'+colorsMassive[color]+'!important;}.border-spec-reverse{border-color:#'+colorsMassiveHover[color]+'!important; }.border-spec-reverse-hover:hover{border-color:#'+colorsMassive[color]+'!important; }.svg-spec{fill:#'+colorsMassive[color]+'!important;}.svg-spec-hover:hover{fill:#'+colorsMassiveHover[color]+'!important;}.svg-spec-reverse{fill:#'+colorsMassiveHover[color]+'!important;}.svg-spec-reverse-hover:hover{fill:#'+colorsMassive[color]+'!important;}</style>';
 
     $('head').append(randomStyles);
 
@@ -92,62 +92,193 @@ function randomColors(){
             backgroundHover:null,
             colorHover:null,
             borderHover:null,
-            fillHover:null
+            fillHover:null,
+            reverseBackground:false,
+            reverseColor:false,
+            reverseBorder:false,
+            reverseFill:false
         }
 
         $.extend(hoverFuncOptions, hoverOptions);
 
         var hoverStyle = '';
+        var secretHoverStyle = '';
         if(hoverFuncOptions.backgroundHover != null){
             hoverStyle = hoverStyle + 'background-color:#'+ hoverFuncOptions.backgroundHover + '!important;';
+            if(!hoverFuncOptions.reverseBackground){
+                secretHoverStyle = secretHoverStyle + 'background-color:#bd00fc!important;';
+            }else{
+                secretHoverStyle = secretHoverStyle + 'background-color:#de8df9!important;';
+            }
         }
         if(hoverFuncOptions.colorHover != null){
             hoverStyle = hoverStyle + 'color:#'+ hoverFuncOptions.colorHover + '!important;';
+            secretHoverStyle = secretHoverStyle + 'color:#bd00fc!important;';
+            if(!hoverFuncOptions.reverseColor){
+                secretHoverStyle = secretHoverStyle + 'color:#bd00fc!important;';
+            }else{
+                secretHoverStyle = secretHoverStyle + 'color:#de8df9!important;';
+            }
         }
         if(hoverFuncOptions.borderHover != null){
             hoverStyle = hoverStyle + 'border-color:#'+ hoverFuncOptions.borderHover + '!important;';
+            secretHoverStyle = secretHoverStyle + 'border-color:#bd00fc!important;';
+            if(!hoverFuncOptions.reverseBorder){
+                secretHoverStyle = secretHoverStyle + 'border-color:#bd00fc!important;';
+            }else{
+                secretHoverStyle = secretHoverStyle + 'border-color:#de8df9!important;';
+            }
         }
         if(hoverFuncOptions.fillHover != null){
             hoverStyle = hoverStyle + 'fill:#'+hoverFuncOptions.fillHover + '!important;';
+            secretHoverStyle = secretHoverStyle + 'fill:#bd00fc!important;';
+            if(!hoverFuncOptions.reverseFill){
+                secretHoverStyle = secretHoverStyle + 'fill:#bd00fc!important;';
+            }else{
+                secretHoverStyle = secretHoverStyle + 'fill:#de8df9!important;';
+            }
         }
 
         var unhoverStyle = '';
+        var secretUnhoverStyle = '';
         if(hoverFuncOptions.backgroundUnhover != null){
             unhoverStyle = unhoverStyle + 'background-color:#'+ hoverFuncOptions.backgroundUnhover + '!important;';
+            secretUnhoverStyle = secretUnhoverStyle + 'background-color:#de8df9!important;';
+            if(!hoverFuncOptions.reverseBackground){
+                secretUnhoverStyle = secretUnhoverStyle + 'background-color:#de8df9!important;';
+            }else{
+                secretUnhoverStyle = secretUnhoverStyle + 'background-color:#bd00fc!important;';
+            }
         }
         if(hoverFuncOptions.colorUnhover != null){
             unhoverStyle = unhoverStyle + 'color:#'+ hoverFuncOptions.colorUnhover + '!important;';
+            secretUnhoverStyle = secretUnhoverStyle + 'color:#de8df9!important;';
+            if(!hoverFuncOptions.reverseColor){
+                secretUnhoverStyle = secretUnhoverStyle + 'color:#de8df9!important;';
+            }else{
+                secretUnhoverStyle = secretUnhoverStyle + 'color:#bd00fc!important;';
+            }
         }
         if(hoverFuncOptions.borderUnhover != null){
             unhoverStyle = unhoverStyle + 'border-color:#'+ hoverFuncOptions.borderUnhover + '!important;';
+            secretUnhoverStyle = secretUnhoverStyle + 'border-color:#de8df9!important;';
+            if(!hoverFuncOptions.reverseBorder){
+                secretUnhoverStyle = secretUnhoverStyle + 'border-color:#de8df9!important;';
+            }else{
+                secretUnhoverStyle = secretUnhoverStyle + 'border-color:#bd00fc!important;';
+            }
         }
         if(hoverFuncOptions.fillUnhover != null){
             unhoverStyle = unhoverStyle + 'fill:#'+hoverFuncOptions.fillUnhover + '!important;';
+            secretUnhoverStyle = secretUnhoverStyle + 'fill:#de8df9!important;';
+            if(!hoverFuncOptions.reverseFill){
+                secretUnhoverStyle = secretUnhoverStyle + 'ill:#de8df9!important;';
+            }else{
+                secretUnhoverStyle = secretUnhoverStyle + 'fill:#bd00fc!important;';
+            }
         }
 
         if(unhoverStyle != ''){
             $(changeItem).attr('style', unhoverStyle);
         }
+        if(unhoverStyle != ''){
+            $(hoverItem).find(changeItem).addClass('mega-hover').attr('data-unhover', unhoverStyle);
+        }
+
+        $(hoverItem).find(changeItem).addClass('mega-hover').attr('data-secret-unhover', secretUnhoverStyle);
+
 
         $(hoverItem).hover(
             function(){
                 if(hoverStyle != ''){
-                    $(this).find(changeItem).attr('style', hoverStyle);
+                    if(!$('body').is('.pink')){
+                        $(this).find(changeItem).attr('style', hoverStyle);
+                    }else{
+                        if(!hoverFuncOptions.reverse){
+                            $(this).find(changeItem).attr('style', secretHoverStyle);
+                        }else{
+                            $(this).find(changeItem).attr('style', secretUnhoverStyle);
+                        }
+                    }
+                }else{
+                    $(this).find(changeItem).removeAttr('style');
                 }
             },
             function(){
                 if(unhoverStyle != ''){
-                    $(this).find(changeItem).attr('style', unhoverStyle);
+                    if(!$('body').is('.pink')){
+                        $(this).find(changeItem).attr('style', unhoverStyle);
+                    }else{
+                        if(!hoverFuncOptions.reverse){
+                            $(this).find(changeItem).attr('style', secretUnhoverStyle);
+                        }else{
+                            $(this).find(changeItem).attr('style', secretHoverStyle);
+                        }
+                    }
+                }else{
+                    $(this).find(changeItem).removeAttr('style');
                 }
             }
         );
 
     }
 
-    specHover('.top-block-button a', '.span-button-arrow', {backgroundUnhover:colorsMassiveHover[color],borderUnhover:colorsMassive[color],backgroundHover:colorsMassive[color],borderHover:colorsMassiveHover[color]})
+    specHover('.top-block-button a', '.span-button-arrow', {backgroundUnhover:colorsMassiveHover[color],borderUnhover:colorsMassive[color],backgroundHover:colorsMassive[color],borderHover:colorsMassiveHover[color], reverseBackground:true})
 
-    specHover('.footer-list a', 'svg', {fillUnhover:'8d8d8d', fillHover:colorsMassiveHover[color]});
+    specHover('.footer-list a', 'svg', {fillHover:colorsMassiveHover[color]});
 
+
+}
+
+// pony secret
+function secret(){
+    var ponyId = 0;
+    var ponyMain = '<img src=images/secret.gif alt="secret-dance" />';
+
+    $('.logo-block').click(function(){
+
+        if(!$('body').is('.pink') && !$('body').is('.pause')){
+
+            ponyId++;
+            if(ponyId == 8){
+                $('body').addClass('pink pause');
+                $('.mega-hover').each(function() {
+                    $(this).attr('style', $(this).data('secret-unhover'));
+                });
+
+                $('.secret-erotic-wrap').addClass('active').prepend(ponyMain);
+                $('meta[name=theme-color]').remove();
+                $('head').append('<meta name="theme-color" content="#E094F8">');
+
+                setTimeout(function(){
+                    $('body').removeClass('pause');
+                },1000);
+
+                ponyId = 0;
+            }
+
+        }
+        else if(!$('body').is('.pause') && $('body').is('.pink')){
+
+            $('body').addClass('pause');
+            $('.secret-erotic-wrap').removeClass('active');
+            setTimeout(function(){
+                $('body').removeClass('pink pause');
+                $('.mega-hover').each(function() {
+                    if($(this).is('[data-unhover]')){
+                        $(this).attr('style', $(this).data('unhover'));
+                    }else{
+                        $(this).removeAttr('style');
+                    }
+                });
+                $('meta[name=theme-color]').remove();
+                $('head').append('<meta name="theme-color" content="#88c5bd">');
+                $('.secret-erotic-wrap img').remove();
+            },400);
+
+        }
+
+    });
 
 }
 
@@ -196,6 +327,8 @@ $(document).ready(function() {
     randomColors();
 
     cutText();
+
+    secret();
 
 });
 
