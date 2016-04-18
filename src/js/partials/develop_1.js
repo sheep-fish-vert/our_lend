d6d1b64ddd8fd7aaed5638473f9a9bac01319bf9
Dmitry <dmitry1939@gmail.com>
2016-04-18 10:20:13 +0300

    poop

--- src/js/partials/develop_1.js
+++ src/js/partials/develop_1.js
@@ -1,73 +1,55 @@
 
-/* function for line parallaxes */
+// secret
+function secret(){
+    var ponyId = 0;
+    var ponyMain = '<img src=images/secret.gif alt="secret-dance" />';
 
-    function parallaxMouseMove(){
+    $('.logo-block').click(function(){
 
-        $('.parallax-layer').each(function(){
+        if(!$('body').is('.pink') && !$('body').is('.pause')){
 
-            var imgHeight = $(this).find('img').height()+20;
+            ponyId++;
+            if(ponyId == 8){
+                $('body').addClass('pink pause');
+                $('.mega-hover').each(function() {
+                    $(this).attr('style', $(this).data('secret-unhover'));
+                });
 
-            var imageSrc = './'+$(this).find('img').attr('src');
-            $(this).css({'height':imgHeight+'px'});
-            $(this).find('span').css({'height':imgHeight+'px', 'background-image':'url('+imageSrc+')'});
+                $('.secret-erotic-wrap').addClass('active').prepend(ponyMain);
+                $('meta[name=theme-color]').remove();
+                $('head').append('<meta name="theme-color" content="#E094F8">');
 
-        });
+                setTimeout(function(){
+                    $('body').removeClass('pause');
+                },1000);
 
-        $('.parallax-block').each(function(index, el) {
-            var centerParallaxBlockY = $(this).height()/2;
-            var centerParallaxBlockX = $(this).width()/2;
-            $(this).attr('data-center-y', centerParallaxBlockY);
-            $(this).attr('data-center-x', centerParallaxBlockX);
-        });
-
-        $(window).resize(function(){
-
-            $('.parallax-layer').each(function(){
-
-                var imgHeight = $(this).find('img').height()+20;
-                $(this).css({'height':imgHeight+'px'});
-                $(this).find('span').css({'height':imgHeight+'px'});
-
-            });
-
-            $('.parallax-block').each(function(index, el) {
-                var centerParallaxBlockY = $(this).height()/2;
-                var centerParallaxBlockX = $(this).width()/2;
-                $(this).attr('data-center-y', centerParallaxBlockY);
-                $(this).attr('data-center-x', centerParallaxBlockX);
-            });
-
-        });
-
-        $('.parallax-block').mousemove(function(e){
-
-            var coordX = e.pageX;
-            var coordY = e.pageY;
-
-            var movedPathX = $(this).data('center-x') - coordX;
-            var movedPathY = null;
-
-            if($(this).is('.y-too')){
-                movedPathY = $(this).data('center-y') - coordY;
+                ponyId = 0;
             }
 
-            $(this).find('.parallax-layer:not(.not-move)').each(function(index, el) {
-                var movedPathWithKoofX = movedPathX * $(this).data('speed');
+        }
+        else if(!$('body').is('.pause') && $('body').is('.pink')){
 
-                if(movedPathY != null){
-                    var movedPathWithKoofY = movedPathY * $(this).data('speed');
-                    $(this).find('span').css({'left': movedPathWithKoofX+'px', 'top': movedPathWithKoofY+'px'});
-                }else{
-                    $(this).find('span').css({'left': movedPathWithKoofX+'px'});
-                }
+            $('body').addClass('pause');
+            $('.secret-erotic-wrap').removeClass('active');
+            setTimeout(function(){
+                $('body').removeClass('pink pause');
+                $('.mega-hover').each(function() {
+                    if($(this).is('[data-unhover]')){
+                        $(this).attr('style', $(this).data('unhover'));
+                    }else{
+                        $(this).removeAttr('style');
+                    }
+                });
+                $('meta[name=theme-color]').remove();
+                $('head').append('<meta name="theme-color" content="#88c5bd">');
+                $('.secret-erotic-wrap img').remove();
+            },400);
 
-            });
+        }
 
-        });
+    });
 
-    }
-
-/* /function for line parallaxes */
+}
 
 $(document).ready(function(){
 
@@ -76,7 +58,7 @@ $(document).ready(function(){
 
 $(window).load(function(){
 
-    parallaxMouseMove();
+    secret();
 
 });
 
