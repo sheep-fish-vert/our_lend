5a418901e8c639acf7a8030bfe7633607131f862
Dmitry <dmitry1939@gmail.com>
2016-04-28 09:25:55 +0300

    removed some magic

--- src/js/validate_script.js
+++ src/js/validate_script.js
@@ -426,17 +426,20 @@ function portfolioPop2(){
 
 function portfolioShowMore(){
     $(document).on('click','.button-show-more button:not(.preload2, .no-more)',function(event){
+
         var itemButton = $(this);
         var id = $(this).data('button-id');
         var page = parseInt($(this).data('page'));
         var elementsPrepage = $(this).data('prepage');
         itemButton.addClass('preload2');
+
         $.ajax({
             url : 'js/json/show_more.json', // show_More
             data: {page:page, elementsPrepage:elementsPrepage},
             success : function(data){
 
-                var dataParsed = JSON.parse(data);
+                var dataParsed = data;
+                //var dataParsed = JSON.parse(data);
 
                 dataParsed.items.forEach(function(item, i, arr){
                     $('.portfolio-wrap').append('<div class="item" data-id='+item.item_id+'><a href="#"><div class="item-img"><img src="'+item.item_img+'" alt=""></div><div class="item-text"><div class="item-text-wrap border-spec"><div class="item-type color-spec"><span>'+item.item_top_text+'</span></div><div class="item-name"><h3>'+item.item_name+'</h3><span class="background-spec"></span></div><div class="item-desc"><p>'+item.item_bottom_text+'</p></div></div></div></a></div>');
